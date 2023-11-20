e2f580f10b6bb92c1cd645979f195929e07343d7
Juan G <me@markski.ar>
2023-11-20 02:07:25 -0300

    dark mode improvements

--- action/switch.php
+++ action/switch.php
@@ -1,4 +1,5 @@
 <?php
+$style_header = '@import "template/style.css";';
 
 if (isset($_COOKIE['preferred_theme']) && $_COOKIE['preferred_theme'] == "dark") {
     setcookie("preferred_theme", "light", time()+(84600*30), "/");
@@ -7,6 +8,7 @@ if (isset($_COOKIE['preferred_theme']) && $_COOKIE['preferred_theme'] == "dark")
 else {
     setcookie("preferred_theme", "dark", time()+(84600*30), "/");
     $_COOKIE['preferred_theme'] = "dark";
+    $style_header .= '@import "template/style_dark.css";';
 }
 
-Header("Location: {$_GET['return']}");
+echo $style_header;
