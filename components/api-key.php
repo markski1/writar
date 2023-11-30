ee98fec47fec3636ffccedc069c26f3208052ed6
Juan G <me@markski.ar>
2023-11-30 19:53:13 -0300

    misc api and page stuff

--- components/api-key.php
+++ components/api-key.php
@@ -5,7 +5,7 @@ $session = new session;
 ?>
 
 <h4>api key</h4>
-<p>api key is used for api access. for more information about the api, <a href="/doc/NotYetMade" hx-get="/doc/NotYetMade" hx-target="main">click here</a>.</p>
+<p>api key is used for api access. for more information about the api, <a href="/doc/D4QjlGqWKP" hx-get="/doc/D4QjlGqWKP" hx-target="main">click here</a>.</p>
 <p>current key:</p>
 <pre><?=$session->get_api_key()?></pre>
 <p><a href="#" hx-get="../action/new-key.php" hx-target="#settings">generate new api key</a></p>
