7d184abab2223fa9f71461920b7add1eb9962f7a
Juan G <me@markski.ar>
2023-12-06 15:41:08 -0300

    SSL for sql

--- data/db.php
+++ data/db.php
@@ -4,7 +4,12 @@ include 'auth/auth.php';
 
 function db_connect(): mysqli
 {
-    get_db_auth($db_host, $db_user, $db_password, $db_name);
+    get_db_auth($db_host, $db_user, $db_password, $db_name, $cert_path);
 
-    return new mysqli($db_host, $db_user, $db_password, $db_name);
+    $mysqli = mysqli_init();
+    $mysqli->ssl_set(NULL, NULL, $cert_path, NULL, NULL);
+    if ($mysqli->real_connect($db_host, $db_user, $db_password, $db_name))
+        return $mysqli;
+    else
+        exit("Cannot connect to database.");
 }
