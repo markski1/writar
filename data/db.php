e7eef037aa6b1d71ccd2331e78b8940f2d29c9dc
Juan G <me@markski.ar>
2023-12-06 22:49:37 -0300

    nevermind

--- data/db.php
+++ data/db.php
@@ -4,10 +4,9 @@ include 'auth/auth.php';
 
 function db_connect(): mysqli
 {
-    get_db_auth($db_host, $db_user, $db_password, $db_name, $cert_path);
+    get_db_auth($db_host, $db_user, $db_password, $db_name);
 
     $mysqli = mysqli_init();
-    $mysqli->ssl_set(NULL, NULL, $cert_path, NULL, NULL);
     if ($mysqli->real_connect($db_host, $db_user, $db_password, $db_name))
         return $mysqli;
     else
