90e8c443056a29e2baa5869b88e5cdc1ef77ef9b
Juan G <me@markski.ar>
2024-01-15 00:51:25 -0300

    HX-Vary header, animations

--- components/api-key.php
+++ components/api-key.php
@@ -5,7 +5,7 @@ $session = new session;
 ?>
 
 <h4>api key</h4>
-<p>api key is used for api access. for more information about the api, <a href="/doc/D4Qjl" hx-get="/doc/D4Qjl" hx-target="main">click here</a>.</p>
+<p>api key is used for api access. for more information about the api, <a href="/doc/D4Qjl" hx-get="/doc/D4Qjl" hx-target="main" hx-indicator='main'>click here</a>.</p>
 <p>current key:</p>
 <pre><?=$session->get_api_key()?></pre>
 <p><a href="#" hx-get="../action/new-key.php" hx-target="#settings">generate new api key</a></p>
--- template/engine.php
+++ template/engine.php
@@ -25,12 +25,13 @@ class Template {
                 $destination_url = $matches[1];
                 $destination_text = $matches[2];
 
-                return "<a href='../{$destination_url}' hx-get='../{$destination_url}' hx-push-url='true' hx-target='main'>{$destination_text}</a>";
+                return "<a href='../{$destination_url}' hx-get='../{$destination_url}' hx-push-url='true' hx-target='main' hx-indicator='main'>{$destination_text}</a>";
             },
             $content
         );
 
         if (isset($_SERVER['HTTP_HX_REQUEST'])) {
+            header('Vary: HX-Request');
             echo $content;
             echo "<script>
                       document.title = '{$this->title} - writar';
